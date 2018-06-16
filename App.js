a1feb3869285b2675d103b1651108a081b916fc6
dorarep <ishikami.naotaka@gmail.com>
2018-06-17 02:14:43 +0900

    use AppLoading until loading images.

--- App.js
+++ App.js
@@ -1,24 +1,40 @@
 import React, { Component }    from 'react';
+import { AppLoading, Asset }   from 'expo';
 import RootNavigator           from 'children/src/navigators/RootNavigator';
 import { YellowBox }           from 'react-native';
 import { Provider }            from 'react-redux';
 import configureStore          from 'children/src/stores';
 import { PersistGate }         from 'redux-persist/integration/react';
 import { ActionSheetProvider } from '@expo/react-native-action-sheet';
-import { cacheAssetsAsync }    from 'children/src/util';
 
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 
 const { store, persistor } = configureStore();
 
 export default class App extends Component {
-  static componentWillMount() {
-    cacheAssetsAsync({
-      images: [require('children/assets/images/child.png')],
-    });
-  }
+  state = {
+    isReady: false,
+  };
+
+  _loadResourcesAsync = async () => {
+    return Promise.all([
+      Asset.loadAsync([
+        require('children/assets/images/child.png')
+      ]),
+    ]);
+  };
 
   render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+
     return (
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
--- src/util/index.js
+++ src/util/index.js
@@ -6,23 +6,3 @@ export const formatTasks = (tasks, childId, works) => {
     return task;
   }).sort((a, b) => a.order - b.order);
 };
-
-export const cacheAssetsAsync = ({images = [], fonts = [], videos = [],}) => {
-  return Promise.all([
-    ...cacheImages(images),
-    ...cacheFonts(fonts),
-    ...cacheVideos(videos),
-  ]);
-}
-
-function cacheImages(images) {
-  return images.map(image => Asset.fromModule(image).downloadAsync());
-}
-
-function cacheVideos(videos) {
-  return videos.map(video => Asset.fromModule(video).downloadAsync());
-}
-
-function cacheFonts(fonts) {
-  return fonts.map(font => Font.loadAsync(font));
-}
