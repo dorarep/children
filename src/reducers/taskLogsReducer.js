ede7427b7fdc432edacd6e4f35425f1d8c8fc797
dorarep <ishikami.naotaka@gmail.com>
2018-07-03 20:17:02 +0900

    update calendar.

--- src/reducers/taskLogsReducer.js
+++ src/reducers/taskLogsReducer.js
@@ -2,26 +2,26 @@ import {
   DELETE_TASK_LOG,
   EXECUTE_TASK,
 } from 'children/src/action_types';
+import { generateUuid } from 'children/src/util';
 
 export default (state = {}, action) => {
   switch (action.type) {
     case DELETE_TASK_LOG:
-      Object.keys(state).forEach((date) => {
-        state[date] = state[date].filter(taskLog => taskLog.taskLogId !== action.id);
-      });
-      return state;
+      return state.filter(taskLog => taskLog.taskLogId !== action.id);
     case EXECUTE_TASK:
       const date       = new Date();
       const dateString = `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`;
       const taskLog = {
-        taskLogId:    date.getTime(),
+        taskLogId:    generateUuid(),
         task:         action.task,
         childId:      action.childId,
+        date:         dateString,
       };
 
-      state[dateString] ? state[dateString].push(taskLog) : state[dateString] = [taskLog];
-
-      return state;
+      return [
+        ...state,
+        taskLog,
+      ];
     default:
       return state;
   }
--- src/screens/CalendarScreen.js
+++ src/screens/CalendarScreen.js
@@ -1,12 +1,41 @@
 import React, { Component }             from 'react';
 import { View }                         from 'react-native';
-import { Agenda }                       from 'react-native-calendars';
-import { ListItem, Icon }               from 'react-native-elements';
+import { Calendar }                     from 'react-native-calendars';
+import { List, ListItem, Icon }         from 'react-native-elements';
 import { connect }                      from 'react-redux';
 import Swipeout                         from 'react-native-swipeout';
 import { deleteTaskLog }                from 'children/src/actions';
 
 class CalendarScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { dateString: '' }
+  }
+
+  formatTaskLog(taskLogs) {
+    let formattedLogs = {};
+    taskLogs.forEach((taskLog) => {
+      formattedLogs[taskLog.date] = [taskLog].concat(formattedLogs[taskLog.date])
+    });
+    return formattedLogs
+  }
+
+  markedDates(taskLogs) {
+    let markedDates = {};
+    taskLogs.forEach((taskLog) => {
+      markedDates[taskLog.date] = {
+        marked: true,
+        selected: this.state.dateString === taskLog.date
+      }
+    });
+    return markedDates
+  }
+
+  selectedLogs(taskLogs) {
+    return taskLogs.filter((taskLog) => taskLog.date === this.state.dateString)
+  }
+
   render() {
     const swipeout = (taskLog) => [
       {
@@ -18,24 +47,46 @@ class CalendarScreen extends Component {
     const getChild = (childId) => this.props.children.reduce((carry, child) => child.id === childId ? child : carry);
 
     return (
-      <Agenda
-        items={this.props.taskLogs}
-        renderItem={(item, firstItemInDay) => (
-          <Swipeout right={swipeout(item)} autoClose={true} backgroundColor='#fff'>
+      <View>
+        <Calendar
+          onDayPress={(dateString) => { this.setState(dateString) }}
+          markedDates={this.markedDates(this.props.taskLogs)}
+        />
+        <List>
+          {this.selectedLogs(this.props.taskLogs).map((item) =>
+          < Swipeout right={swipeout(item)} autoClose={true} backgroundColor='#fff'>
             <ListItem
-              roundAvatar
-              avatar={<Icon name={item.task.work.icon} />}
-              title={item.task.work.name}
-              subtitle={`${item.task.work.point}ポイント`}
-              rightTitle={getChild(item.childId).name}
+            roundAvatar
+            avatar={<Icon name={item.task.work.icon} />}
+            title={item.task.work.name}
+            subtitle={`${item.task.work.point}ポイント`}
+            rightTitle={getChild(item.childId).name}
             />
-          </Swipeout>
-        )}
-        renderEmptyDate={() => <View />}
-        renderEmptyData={() => <View />}
-        rowHasChanged={(r1, r2) => { return r1.id !== r2.id }}
-      />
+            </Swipeout>
+          )}
+        </List>
+      </View>
     );
+    /*
+     <Agenda
+     items={this.formatTaskLog(this.props.taskLogs)}
+     renderItem={(item, firstItemInDay) => (
+     <View> { firstItemInDay ? <View /> :
+     <Swipeout right={swipeout(item)} autoClose={true} backgroundColor='#fff'>
+     <ListItem
+     roundAvatar
+     avatar={<Icon name={item.task.work.icon} />}
+     title={item.task.work.name}
+     subtitle={`${item.task.work.point}ポイント`}
+     rightTitle={getChild(item.childId).name}
+     />
+     </Swipeout>
+     } </View>
+     )}
+     renderEmptyDate={() => <View />}
+     renderEmptyData={() => <View />}
+     rowHasChanged={(r1, r2) => { return r1.id !== r2.id }}
+     /> */
   }
 }
 
